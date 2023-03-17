3f0ce1c5525c0188447ccbffc1c1c699b4cae038
mnc <120148833+manon2546@users.noreply.github.com>
2023-03-17 09:08:23 +0700

    Add files via upload
    The Software Engineering project and Group Hai Choa Rod submission

---
+++ login-nextproject/pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions = {
+
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+
+                const res = await fetch("http://127.0.0.1:8080/login", {
+                    method: 'POST',
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" }
+                })
+                const data = await res.json()
+                if(data.status == 'ok'){
+                    return data.user
+                }
+                return null
+            }
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            }),
+    ]
+}
+export default NextAuth(authOptions)
